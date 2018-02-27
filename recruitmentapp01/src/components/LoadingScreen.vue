8d32b553509f8c387b007b57fb9611694874ba5f
procha <procha@bliss.pt>
2018-02-27 13:24:24 +0000

    Added get server health request

--- recruitmentapp01/src/components/LoadingScreen.vue
+++ recruitmentapp01/src/components/LoadingScreen.vue
@@ -1,21 +1,59 @@
+
+
 <template>
 
   <section class="loading-screen">
-    <h1>loading-screen Component</h1>
-    <vue-simple-spinner></vue-simple-spinner>
+
+     <div v-if ="checking == true">
+         <hollow-dots-spinner
+          :animation-duration="1000"
+          :dot-size="20"
+          :dots-num="3"
+          color="#668cff"/>
+    </div>
+    
+    <div v-if ="status == 'OK' ">
+    <at-button type="text">Text Button</at-button>
+
+    </div>
+  
+
   </section>
 
 </template>
 
 <script>
- 
+  import { HollowDotsSpinner } from 'epic-spinners';
+  import axios from 'axios';
+
   export default  {
+    components: {
+      //spinner shown while checking server health
+      HollowDotsSpinner
+    },
     name: 'LoadingScreen',
     data() {
+      var status = "OK";
+      var checking = true;
       return {
+        status,
+        checking
+      }
+    },
+    mounted:function(){
+        this.getServerHealth();
+    },
+    methods : {
+      getServerHealth: function(){
+        // request to check server health
+        axios.get(`https://private-anon-08ab44f3c8-blissrecruitmentapi.apiary-mock.com/health`).then(response => {
+          this.status = response.data.status;
+        })
+        .catch(e => {this.status="ERROR";});
+        setTimeout(() => { this.checking = false; }, 3000)
       }
     }
-}
+  }   
 </script>
 
 <style scoped >
@@ -25,4 +63,6 @@
      left: 50%;
      transform: translate(-50%, -50%);
   }
+
+  
 </style>
--- recruitmentapp01/src/router/index.js
+++ recruitmentapp01/src/router/index.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import LoadingScreen from '@/components/LoadingScreen'
 
-
 Vue.use(Router)
+
+
+
 
 export default new Router({
   routes: [
